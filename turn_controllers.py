0f3fdef260204010493cdfc65974fa6cd9ec13bd
VahidGhafourian <vahidghafourianam@gmail.com>
2023-09-10 22:56:40 +0330

    Fix new rule bug

--- turn_controllers.py
+++ turn_controllers.py
@@ -28,7 +28,7 @@ def calculate_score(main_game):
 def check_finish(main_game):
     # check if the game pass the minimum number of turns
     if main_game.turn_number < int(main_game.config["minimum_number_of_turns"]):
-        return
+        return False
 
     # find the number of strategic nodes for each player
     players_strategic_nodes_count = []
